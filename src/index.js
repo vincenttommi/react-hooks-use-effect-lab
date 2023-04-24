e9c30c909a6bf38de4cdc90d75d9626beb511132
vincenttommi <vincenttommi@gmail.com>
2023-04-24 10:03:55 +0300

    dummy lab

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
-
+// //gi
 ReactDOM.render(<App />, document.getElementById("root"));
